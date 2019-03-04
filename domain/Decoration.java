76033365cd028acad02d9e9d1cda177d64fa6e61
ArnauPla <45559064+ArnauPla@users.noreply.github.com>
2019-03-04 11:58:08 +0100

    Add files via upload

---
+++ domain/Decoration.java
@@ -0,0 +1,14 @@
+package com.flowershop.domain;
+
+public class Decoration {
+
+	protected double price;
+	protected String material;
+	
+	public Decoration(double price, String material) {
+		super();
+		this.price = price;
+		this.material = material;
+	}
+	
+}
